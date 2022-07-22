5e5975bed2f1ea0388aeaeebfc36d0909b62bb83
ArianaRusso <arianarusso@hotmail.com>
2022-07-22 15:01:57 -0300

    mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.arianarusso.desafio.dominio.Curso;
+import br.com.arianarusso.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1= new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("curso sobre java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2= new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("curso sobre JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria= new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
